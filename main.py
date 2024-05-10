3f02c1be362cb73b03e6e728ac3204d1de6315be
Yousef360 <lover.avenger.9@gmail.com>
2024-05-10 23:04:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import streamlit
+import streamlit as st
 from Search_Engine import SearchEngine
 
 text_input = st.text_input("Search bar", value=None, max_chars=256, key="text1", placeholder="بتدوّر على إيه ؟")
