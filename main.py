38ddc70728a30244029d9148233ec31064ff0651
Yousef360 <lover.avenger.9@gmail.com>
2024-05-11 01:52:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ if "my_engine" not in st.session_state:
 text_input = st.text_input("Search bar", value=None, max_chars=256, key="text1", placeholder="بتدوّر على إيه ؟")
 button1_clicked = st.button("Search", key='button1')
 if button1_clicked:
-    result = st.session_state['my_engine'].find(text_input, 2)
+    with st.spinner("Searching ..."):
+        result = st.session_state['my_engine'].find(text_input, 2)
     for i in range(len(result)):
         with st.expander(f"See Result {i + 1}"):
             st.write(result[i])
