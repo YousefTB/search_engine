c548526531e1fa84c11b240eefed4610aac22670
Yousef360 <lover.avenger.9@gmail.com>
2024-05-10 23:18:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import streamlit as st
 from Search_Engine import SearchEngine
 
-text_input = st.text_input("Search bar", value=None, max_chars=256, key="text1", placeholder="هنيك شعبك يا اسكندراني يا عرص")
+if "my_engine" not in st.session_state:
+    st.session_state['my_engine'] = SearchEngine()
+    
+
+text_input = st.text_input("Search bar", value=None, max_chars=256, key="text1", placeholder="بتدوّر على إيه ؟")
+button1_clicked = st.button("Search", key='button1')
