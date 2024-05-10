8245b83e986b26df0a1e2675ed6962f753218c81
Yousef360 <lover.avenger.9@gmail.com>
2024-05-11 01:39:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ if "my_engine" not in st.session_state:
 text_input = st.text_input("Search bar", value=None, max_chars=256, key="text1", placeholder="بتدوّر على إيه ؟")
 button1_clicked = st.button("Search", key='button1')
 if button1_clicked:
-    st.session_state['my_engine'].find(text_input, 1)
+    st.session_state['my_engine'].find(text_input, 2)
